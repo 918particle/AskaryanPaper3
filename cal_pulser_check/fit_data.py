a4d1632c3435d91de68dea735292dfbea7b64227
Jordan C hanson <918particle@gmail.com>
2026-03-19 18:20:40 -0700

    wrote a lot, and added cal-pulser section

--- cal_pulser_check/fit_data.py
+++ cal_pulser_check/fit_data.py
@@ -18,7 +18,7 @@ n_channels = 7
 data = utility.load_csv_data(open(args.filename,newline=''),trace_length,n_channels)
 csw = utility.get_csw(data,2,[0,1,2,3,4,5,6])
 times = np.arange(0,trace_length)/fs
-f0 = 0.190
+f0 = 0.19
 gamma = 0.02
 secondary_amplitude = 0.01
 plot_delay = 106
@@ -29,42 +29,19 @@ if(args.first_sigma and args.second_sigma and args.the_delay and args.the_amplit
 		model[i] = utility.math_conv(times[i]-plot_delay,1,1,args.first_sigma,f0,gamma)
 		model[i] += utility.math_conv(times[i]-args.the_delay-plot_delay,1,1,args.second_sigma,f0,gamma)*args.the_amplitude
 	model = model/np.sqrt(np.inner(model,model))
-	#print(np.max(np.abs(correlate(model,csw))))
+	print(np.max(np.abs(correlate(model,csw))))
 	for i in range(trace_length):
 		print(times[i],csw[i],model[i])
 else:
 	rho_max = 0
 	sigmas = np.arange(0.3,3.0,0.1)
-	delays = np.arange(22,28,0.25)
-	amplitudes = np.arange(0.01,0.5,0.05)
 	best_sigma_main = 0
-	best_sigma_reflection = 0
-	best_delay = 0
-	best_deltaP = 1e6
-	best_amplitude = 0
-	for delay in delays:
-		for sigma_1 in sigmas:
-			for sigma_2 in sigmas:
-				if(sigma_2<sigma_1):
-					continue
-				else:
-					for i in range(trace_length):
-						model[i] = utility.math_conv(times[i],1,1,sigma_1,f0,gamma)
-						model[i] += utility.math_conv(times[i]-delay,1,1,sigma_2,f0,gamma)*secondary_amplitude
-					model = model/np.sqrt(np.inner(model,model))
-					rho = np.max(np.abs(correlate(model,csw)))
-					if(rho>rho_max):
-						best_sigma_main=sigma_1
-						best_sigma_reflection=sigma_2
-						best_delay = delay
-						rho_max = rho
-	for amp in amplitudes:
+	for sigma_1 in sigmas:
 		for i in range(trace_length):
-			model[i] = utility.math_conv(times[i]-plot_delay,1,1,best_sigma_main,f0,gamma)
-			model[i] += utility.math_conv(times[i]-best_delay-plot_delay,1,1,best_sigma_reflection,f0,gamma)*amp
+			model[i] = utility.math_conv(times[i],1,1,sigma_1,f0,gamma)
 		model = model/np.sqrt(np.inner(model,model))
-		deltaP = np.mean(np.square(model-csw))
-		if(deltaP<best_deltaP):
-			best_deltaP = deltaP
-			best_amplitude = amp
-	print(rho_max,best_sigma_main,best_sigma_reflection,best_delay,best_amplitude)
+		rho = np.max(np.abs(correlate(model,csw)))
+		if(rho>rho_max):
+			best_sigma_main=sigma_1
+			rho_max = rho
+	print(rho_max,best_sigma_main)
---
+++ cal_pulser_check/run_all.sh
@@ -0,0 +1,7 @@
+python3 fit_data.py 1995_39.csv
+python3 fit_data.py 2250_1064.csv
+python3 fit_data.py 2250_1072.csv
+python3 fit_data.py 2250_1081.csv
+python3 fit_data.py 2250_1088.csv
+python3 fit_data.py 2250_1107.csv
+python3 fit_data.py 2250_1119.csv
